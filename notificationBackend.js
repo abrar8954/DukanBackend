7e1730fd0aaef44a7468bd0f6dbd54aeae1dbec6
Abrar Sardar <abrarsardar8955@gmail.com>
2023-07-23 15:03:13 +0500

    notification backend

---
+++ notificationBackend.js
@@ -0,0 +1,34 @@
+const admin = require('firebase-admin')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+var serviceAccount = require("./dukanfiba-firebase-adminsdk-6rokc-7d2bbfdd2c.json");
+app.use(express.json())
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+app.post('/send-noti', (req, res) => {
+    console.log('req.body: ', req.body)
+    
+    const message = {
+        data: {
+            title: req.body.name,
+            body: req.body.msg,
+            largeIcon: req.body.image,
+        },
+        tokens: req.body.token
+
+    }
+
+    admin.messaging().send(message).then(res => {
+        console.log('send success')
+    }).catch(err => {
+        console.log(err)
+    })
+})
+
+app.listen(port, () => {
+    console.log('server running')
+})
